966b5c8112be2af72c5b58ebeb0666980186d9fd
Miguel Uranga <57147319+Lemonaeds@users.noreply.github.com>
2024-11-18 20:55:46 +0000

    Creation of the To-Do list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [input, setInput] = useState("");
+	const [cantTareas, setCantTareas] = useState([]);
+	
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<p>My To-Dos</p>
+			<ul>
+				<li id = "listItems">
+					<input className="text-body-secondary"
+						type="taskAdder" 
+						placeholder="Add a task..." 
+						onChange={(e) => setInput(e.target.value)}
+						value = {input}
+						onKeyPress={(e) => {
+							console.log("Key pressed", e.key);
+							if(e.key == "Enter" )
+								setCantTareas(cantTareas.concat(input)) 	
+						}}
+						/>
+				</li>
+				{cantTareas.map((item, index)=>(
+					<li id = "listItems"> 
+						<div className="container d-flex justify-content-between text-body-secondary">
+							{item} <i 
+										id= "exFontAwesome"
+										className="fa-solid fa-x mt-3"
+										style={{"text-align-last":"right;"}}
+										onClick={() => setCantTareas(
+												cantTareas.filter(
+													(t, currentIndex) => index != currentIndex)
+													)}>
+
+							</i>
+						</div>
+						
+					</li>
+				))}
+				<li>{cantTareas.length != 0 ? cantTareas.length : ""} {cantTareas.length != 0 ? "item(s) left" : "No items yet"}</li>
+				
+			</ul>
+
 		</div>
+
+		
 	);
 };
 
